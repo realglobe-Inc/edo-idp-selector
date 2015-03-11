realglobe-Inc/edo-id-provider

データアクセスラッパーに close メソッドを付けた

server_test.go
@@ -95,6 +95,7 @@ func TestBoot(t *testing.T) {
 	if err != nil {
 		t.Fatal(err)
 	}
+	defer idpSys.close()
 	defer os.RemoveAll(idpSys.uiPath)
 	defer func() { shutCh <- struct{}{} }()
 
@@ -602,6 +603,7 @@ func TestSuccess(t *testing.T) {
 		t.Fatal(err)
 	}
 	defer taServ.Close()
+	defer idpSys.close()
 	defer os.RemoveAll(idpSys.uiPath)
 	defer func() { shutCh <- struct{}{} }()
 	// TA にリダイレクトしたときのレスポンスを設定しておく。
@@ -662,6 +664,7 @@ func TestAbortSession(t *testing.T) {
 		t.Fatal(err)
 	}
 	defer taServ.Close()
+	defer idpSys.close()
 	defer os.RemoveAll(idpSys.uiPath)
 	defer func() { shutCh <- struct{}{} }()
 	// TA にリダイレクトしたときのレスポンスを設定しておく。

realglobe-Inc/edo-idp-selector

server_test.go
@@ -46,6 +46,7 @@ func TestSelectPage(t *testing.T) {
 	}
 
 	sys := newTestSystem()
+	defer sys.close()
 	defer os.RemoveAll(sys.uiPath)
 	shutCh := make(chan struct{}, 10)
 	defer func() { shutCh <- struct{}{} }()
@@ -89,6 +90,7 @@ func TestListPage(t *testing.T) {
 	}
 
 	sys := newTestSystem()
+	defer sys.close()
 	defer os.RemoveAll(sys.uiPath)
 	shutCh := make(chan struct{}, 10)
 	defer func() { shutCh <- struct{}{} }()
@@ -137,6 +139,7 @@ func TestRedirectPage(t *testing.T) {
 	}
 
 	sys := newTestSystem()
+	defer sys.close()
 	defer os.RemoveAll(sys.uiPath)
 	shutCh := make(chan struct{}, 10)
 	defer func() { shutCh <- struct{}{} }()