realglobe-Inc/edo-id-provider

edo から edo-toolkit へのリポジトリ名変更に対応

log.go
@@ -4,7 +4,7 @@ import (
 	"github.com/realglobe-Inc/go-lib/rglog"
 )
 
-var log = rglog.Logger("github.com/realglobe-Inc/edo/edo-id-provider")
+var log = rglog.Logger("github.com/realglobe-Inc/edo-toolkit/edo-id-provider")
 
 // ログにそのまま書くのが憚られるので隠す。
 func mosaic(str string) string {

realglobe-Inc/edo-idp-selector

log.go
@@ -4,4 +4,4 @@ import (
 	"github.com/realglobe-Inc/go-lib/rglog"
 )
 
-var log = rglog.Logger("github.com/realglobe-Inc/edo/edo-idp-selector")
+var log = rglog.Logger("github.com/realglobe-Inc/edo-toolkit/edo-idp-selector")