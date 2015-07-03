c18cdf883fafaef38a3336ec78d9be25c2dced36
fukuchidaisuke <fukuchidaisuke@realglobe.jp>
2015-07-03 14:08:33 +0900

    ログラベルを修正

--- page/idpselect/log.go
+++ page/idpselect/log.go
@@ -18,4 +18,4 @@ import (
 	"github.com/realglobe-Inc/go-lib/rglog"
 )
 
-var log = rglog.Logger("github.com/realglobe-Inc/edo-idp-selector/page")
+var log = rglog.Logger("github.com/realglobe-Inc/edo-idp-selector/page/idpselect")
